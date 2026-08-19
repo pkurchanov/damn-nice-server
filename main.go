d7f94ff9866733b5d286503651b5a89dca53f7c3
greemfox <pavelkurchanov.lpk@gmail.com>
2026-08-19 21:23:41 +0700

    start hacking away

--- main.go
+++ main.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+// So here's a byte arr the size of a standard DNS packet
+// and a way of keeping track of where we are. Shrimple.
+type BytePacketBuffer struct {
+	buf [512]uint8
+	pos uint
+}
+
 func main() {
-	fmt.Println("blah")
+	// Missing fields will default to their zero values.
+	buffy := BytePacketBuffer{}
+	fmt.Println("default position:", buffy.pos)
+	fmt.Println("first cell:", buffy.buf[0])
+	fmt.Println("last cell:", buffy.buf[511])
 }
