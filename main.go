0bec20d8f835975037bc54f43fb983e239082745
greemfox <pavelkurchanov.lpk@gmail.com>
2026-08-20 21:02:24 +0700

    fix get()

--- main.go
+++ main.go
@@ -5,45 +5,45 @@ import "fmt"
 // So here's a byte arr the size of a standard DNS packet
 // and a way of keeping track of where we are. Shrimple.
 type BytePacketBuffer struct {
-	buf [512]uint8
-	pos uint
+	buffer   [512]uint8
+	position uint
 }
 
 // Step forward a given number of steps
 func (buf *BytePacketBuffer) step(steps uint) {
-	buf.pos += steps
+	buf.position += steps
 }
 
 // Jump to a given position
 func (buf *BytePacketBuffer) seek(pos uint) {
-	buf.pos = pos
+	buf.position = pos
 }
 
 // Read a single byte and step forward
 func (buf *BytePacketBuffer) read() (uint8, error) {
-	if buf.pos > 511 {
-		return 0, fmt.Errorf("end of buffer.")
+	if buf.position > 511 {
+		return 0, fmt.Errorf("end of buffer\n")
 	}
-	res := buf.buf[buf.pos]
-	buf.pos++
+	res := buf.buffer[buf.position]
+	buf.position++
 	return res, nil
 }
 
 // Get a single byte without changing position
-func (buf *BytePacketBuffer) get() (uint8, error) {
-	if buf.pos > 511 {
-		return 0, fmt.Errorf("end of buffer.")
+func (buf *BytePacketBuffer) get(pos uint) (uint8, error) {
+	if pos > 511 {
+		return 0, fmt.Errorf("end of buffer\n")
 	}
-	return buf.buf[buf.pos], nil
+	return buf.buffer[pos], nil
 }
 
 // Get a range of bytes
 func (buf *BytePacketBuffer) getRange(start uint, len uint) ([]uint8, error) {
 	end := start + len
 	if end > 511 {
-		return nil, fmt.Errorf("end of buffer.")
+		return nil, fmt.Errorf("end of buffer\n")
 	}
-	return buf.buf[start:end], nil
+	return buf.buffer[start:end], nil
 }
 
 // Read a two-byte number
@@ -84,17 +84,37 @@ func (buf *BytePacketBuffer) readUint32() (uint32, error) {
 		nil
 }
 
+func (buf *BytePacketBuffer) readQueryName(outstr *string) error {
+	// These are all meant to make dealing with jumps possible
+	pos := buf.position
+	jumped := false
+	const maxJumps = 5
+	jumpsPerformed := 0
+
+	delimiter := ""
+	for true {
+		// I don't think you should reasonably need more than 3, for that matter
+		if jumpsPerformed > 5 {
+			return fmt.Errorf("Jump limit of %d exceeded", maxJumps)
+		}
+
+		// At this point we're looking at the length byte of some label
+		len, err := buf.get(pos)
+		if err != nil {
+			return fmt.Errorf("Couldn't read a label: %s", err)
+		}
+
+		// Two MSB set <=> jump to the offset given by the remaining 6+8=14 bits
+		if (len & 0xC0) == 0xC0 {
+			if !jumped {
+				buf.seek(pos + 2)
+			}
+
+		}
+
+	}
+}
+
 func main() {
-	buffy := BytePacketBuffer{}
-	buffy.buf[0] = 0xC0
-	buffy.buf[1] = 0x0C
-	buffy.buf[2] = 0x0D
-	buffy.buf[3] = 0x0E
-
-	num1, _ := buffy.readUint16()
-	fmt.Println("first two bytes:", num1)
-
-	buffy.seek(0)
-	num2, _ := buffy.readUint32()
-	fmt.Println("first four bytes:", num2)
+	fmt.Println("bottom text")
 }
